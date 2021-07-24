daa841cbcde6264b303b3a21fa3267ba9f609ff6
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-07-23 21:33:49 -0700

    Section 3 completed

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express();//invoke express - instantiating
 const mongoose = require('mongoose');
 //import routes
 const userRoutes = require('./routes/user');
-
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 //db connection
 mongoose.connect(
     process.env.DATABASE,
     {useNewUrlParser: true,
-     useCreateIndex: true
+     useCreateIndex: true,
+     useUnifiedTopology: true
     }
 )
 .then(()=> console.log("DB connected"))
@@ -18,8 +22,23 @@ mongoose.connect(
 mongoose.connection.on('error', err=>{
     console.log(`DB connecton error: ${err.message}`);
 })
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(expressValidator());
 //routes middleware
+
+
 app.use('/api',userRoutes);
+
+
+
+
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.raw());
+app.use(cookieParser());
 
 const port = process.env.PORT || 8000;
 
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const {signup, signin, signout} = require('../controllers/user');
+ const {userSignupValidator} = require('../validator');
 
-
-router.get('/', (req, res) =>{
-
-res.send('Welcome');
-
-})
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
 
 module.exports = router;
