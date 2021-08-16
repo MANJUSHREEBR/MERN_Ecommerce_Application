02c8c671d7078d8a07787eb27034b91d2058edcc
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-08-15 22:53:12 -0700

    After section 10

--- ecommerce-front/src/core/Menu.js
+++ ecommerce-front/src/core/Menu.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {Fragment}from 'react'
 import{Link, withRouter}  from 'react-router-dom'
+import {signout, isAuthenticated} from '../auth'
 
 const isActive = (history, path) => {
 
@@ -18,12 +19,44 @@ const Menu = (props) => (
     <li className="nav-item">
         <Link className="nav-link" style = {isActive(props.history, '/')} to="/">Home</Link>
     </li>
-    <li className="nav-item">
-        <Link className="nav-link" style = {isActive(props.history, '/Signup')}  to="/Signup">Signup</Link>
+   {isAuthenticated() && isAuthenticated().user.role == 0 && (
+        <li className="nav-item">
+        <Link className="nav-link" style = {isActive(props.history, '/user/dashboard')} to="/user/dashboard">Dashboard</Link>
     </li>
-    <li className="nav-item">
-        <Link className="nav-link" style = {isActive(props.history, '/Signin')} to="/Signin">Signin</Link>
+   )}
+    {isAuthenticated() && isAuthenticated().user.role == 1 && (
+        <li className="nav-item">
+        <Link className="nav-link" style = {isActive(props.history, '/admin/dashboard')} to="/admin/dashboard">Dashboard</Link>
     </li>
+   )}
+    {!isAuthenticated() && (
+        <Fragment>
+ <li className="nav-item">
+ <Link className="nav-link" style = {isActive(props.history, '/Signup')}  to="/Signup">Signup</Link>
+</li>
+<li className="nav-item">
+ <Link className="nav-link" style = {isActive(props.history, '/Signin')} to="/Signin">Signin</Link>
+</li>
+
+        </Fragment>
+
+
+
+    )}
+
+   {isAuthenticated() && (
+<Fragment>
+<li className="nav-item">
+        <span className="nav-link" style = {{cursor:'pointer', color: '#ffffff'}} onClick= {() => signout(()=>{ props.history.push("/")})}>
+            Signout</span>
+    </li>
+
+
+</Fragment>
+
+
+   )}
+
 </ul>
 
 </div>
--- ecommerce-front/src/user/Signin.js
+++ ecommerce-front/src/user/Signin.js
@@ -1,16 +1,103 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Link, Redirect} from 'react-router-dom'
 import Layout from '../core/Layout'
+import { signin,authenticate, isAuthenticated} from '../auth'
 
-const Signin = () => (<div>
-  <Layout title="signin Page" description = "Sign in to Node React E-Commerce App">
+const Signin = () => {
+
+const[values,setValues] = useState({
+    email:'manjushree978@gmail.com',
+    password:'Manju@123',
+    error:'',
+    loading: false,
+    redirectToReferrer: false
+})
+const { email, password, error, loading, redirectToReferrer} = values
+const {user} = isAuthenticated();
+
+const handleChange = name => event =>{
+
+  setValues({...values, error: false, [name]: event.target.value})
+
+}
+
+const clickSubmit =(event) => {
+  event.preventDefault();
+  setValues({...values, error: false, loading: true})
+  signin({email,password})
+  .then(data=>{
+
+    if(data.error){
+      setValues({...values, error: data.error, loading: false})
+    }else{
+      authenticate(data,
+        () => setValues({...values, loading:false,redirectToReferrer: true}))
+    }
+  })
+
+}
+
+const signUpForm = () =>(
+  <form>
+    <div className = "form-group">
+      <label className = "text-muted"> Email</label>
+      <input onChange={handleChange('email')} type="email" className="form-control" value={email}/>
+    </div>
+    <div className = "form-group">
+      <label className = "text-muted"> Password</label>
+      <input onChange={handleChange('password')}  type="password" className="form-control" value={password}/>
+    </div>
+    <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+  </form>
+
+
+)
+
+const showError =() => (
+
+
+  <div className="alert alert-danger" style={{display: error ? '' :'none'}}>
+
+    {error}
+  </div>
+)
+const showLoading =() => (
+loading && (<div className="alert alert-info">
+  <h2>Loading...</h2>
+</div>)
+)
+const redirectUser = () => {
+if(redirectToReferrer){
+  if(user && user.role === 1){
+    return <Redirect to="/admin/dashboard" />
+  }
+  else{
+    return <Redirect to="/user/dashboard"/>
+  }
+}
+if(isAuthenticated()){
+  return <Redirect to="/" />
+}
+
+}
+return(
+<Layout title="Signin Page" description = "Sign up to Node React E-Commerce App" className="container col-md-8 offset-md-4">
   
+
+ {showLoading()} 
+ {showError()}
+ {signUpForm()}
+{redirectUser()}
+
+
   
   </Layout>
-  </div>)
 
+
+
+)
+
+}
 
 
 export default Signin;
-
-
-
